c86cbe2afc95729316eb9db3ac12eb34eba33d35
Matías Zozaya <matias.m.zozaya@gmail.com>
2023-04-18 21:57:10 -0300

    Code and files update
    Minor layout changes.
    Unnecessary code removed.

--- js/site.js
+++
@@ -1,7 +0,0 @@
-$("#hero").css("margin-top", "-" + ($("#top-links").height()) + "px");
-
-$(document).ready(function () {
-    $(window).on("resize", function () {
-        $("#hero").css("margin-top", "-" + ($("#top-links").height()) + "px");
-    });
-});
