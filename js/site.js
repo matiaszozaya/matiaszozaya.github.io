47279857007f1313f49c3a9c361499e2144d88a2
Matías Zozaya <68297766+matiaszozaya@users.noreply.github.com>
2026-02-24 23:15:39 -0300

    Entire site rework.

--- js/site.js
+++
@@ -1,7 +0,0 @@
-$("main").css("min-height", "calc(100% - " + ($("header").height() + $("footer").height()) + "px)");
-
-$(document).ready(function () {
-    $(window).on("resize", function () {
-        $("main").css("min-height", "calc(100% - " + ($("header").height() + $("footer").height()) + "px)");
-    });
-});
